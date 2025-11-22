1cf9b40fd70f7b75e1653d029a99b713f3827929
Omar Afify <omaradelafify2003@gmai.com>
2025-11-22 03:07:49 -0500

    style: improve contact page email handle display and refine element styling.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,7 +15,12 @@ const socials = [
 		icon: <Mail size={20} />,
 		href: "mailto:omaradelafify2003@gmail.com",
 		label: "Email",
-		handle: "omaradelafify2003@gmail.com",
+		handle: (
+			<span className="flex flex-col items-center">
+				<span>omaradelafify2003@</span>
+				<span>gmail.com</span>
+			</span>
+		),
 	},
 	{
 		icon: <Github size={20} />,
@@ -32,21 +37,21 @@ export default function Example() {
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
 					{socials.map((s) => (
-						<Card key={s.label}>
+						<Card>
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16 h-full justify-center"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
 								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent top-0"
+									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
 									aria-hidden="true"
 								/>
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
 								</span>{" "}
-								<div className="z-10 flex flex-col items-center text-center">
-									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display break-all">
+								<div className="z-10 flex flex-col items-center">
+									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
 										{s.handle}
 									</span>
 									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
